84b4a1e0bdaa2b1a16883a7b601ba09a8c03730d
Andrés Lou <lourios.andres@gmail.com>
2015-11-26 23:40:38 -0500

    Starting from scratch

--- Andrés' part/CellList.java
+++ Andrés' part/CellList.java
@@ -1,72 +1,83 @@
 public class CellList{
-	private CellNode head;
-	private int size;
 	
-	}
-	//default constructor of CellList
-	public CellList(){
-		head = null;
-		size = 0;
-	}
-
-	//copy constructor of CellList. Unsure about where head is supposed to point.
-	public CellList(CellList cellList){
-		this.head = cellList.head;
-		this.size = cellList.size;
-	}
-
-	public void addToStart(CellPhone cellPhone){
-		CellNode newnode = new CellNode(cellPhone, node);
-	}
-
-	//begin inner class
+	/////////////////////
+	//BEGIN INNER CLASS//
+	/////////////////////
 	public class CellNode{
 		private CellPhone phone;
 		private CellNode link;
 
-		//default constructor
 		public CellNode(){
-			this.phone = null;
-			this.link = null;
+			phone = null;
+			link = null;
 		}
 
-		//parametrised constructor
-		public CellNode(CellPhone phone, CellNode linkValue){
-			this.phone = phone.clone(phone.getSerial());
-			this.link = linkValue;
-		}
-
-		//copy constructor
-		public CellNode(CellNode othernode){
-			this.phone = othernode.getPhone().clone(otherPhone.getPhone().getSerial());
-			this.link = othernode.getLink(); 
-		}
-
-		//clone method
-		public CellNode clone(){
-			CellNode clonednode = new CellNode(this.getPhone(), this.getLink());
-			return clonednode;
-		}
-
-		//accessors and mutators
-
-		//getPhone returns the pointer of the phone object, not the object itself
-		//we have to make sure this is appropriate	
-		public CellPhone getPhone(){
-			return this.phone;
-		}
-
-		public CellNode getLink(){
-			return this.link;
-		}
-
-		//again should the parameter set the pointer or should it create
-		//an entire, identical object?
-		public void setPhone(CellPhone phone){
+		public CellNode(CellPhone phone, CellNode link){
 			this.phone = phone;
+			this.link = link;
 		}
 
-		public void setLink(CellNode link){
-			this.link = link;
-		}	
+		//TO DO: copy constructor and clone methods
+	}
+	///////////////////
+	//END INNER CLASS//
+	///////////////////
+	
+	private CellNode head;
+	private int size;
+
+	//default constructor
+
+	public CellList(){
+		head = null;
+	}
+
+	//TO DO: copy constructor of CellList
+
+	//addToStart() adds a CellPhone object at the beginning of the list
+	public void addToStart(CellPhone cellPhone){
+		head = new CellNode(cellPhone, head);
+	}
+
+	//size() returns the number of nodes in the list
+	public int size(){
+		int count = 0;
+		CellNode position = head;
+
+		while (position != null){
+			count++;
+			position = position.link;
+		}
+
+		return count;
+	}
+
+	//deleteFromStart() deletes the head node and returns true if the list
+	//contains at least one node. Returns false if the list is empty.
+	public boolean deleteFromStart(){
+		if (head != null){
+			head = head.link;
+			return true;
+		}
+
+		return false;
+	}
+
+	//find takes a CellPhone serial number and returns the pointer
+	//of the node that matches it. Returns null if the serial is not
+	//found
+	public CellNode find(long serial){
+		CellNode position = head;
+		long serialAtPostion;
+		while (position != null){
+			serialAtPostion = position.phone.getSerialNum();
+			if (serialAtPostion == serial){
+				return position;
+			}
+			position = position.link;
+		}
+		return null;
+	}
+
+
 }
