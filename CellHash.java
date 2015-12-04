f3ded6a7452c985912572496fc9f9e5cb052942f
lauraelena33 <Lauraelena@Laura-Elena-2.local>
2015-12-04 11:53:37 -0500

    PArt 2 pretty much done: Comments and descriptions added. Jsut need to adjust find methods and explain potential privacy leaks for Part 1.

--- CellHash.java
+++ CellHash.java
@@ -1,3 +1,21 @@
+//--------------------------------------------------------------------------------
+//Assignment 3
+//Part 2
+//Written by: 
+//Sviki Gabbay - 27490968
+//Andrés Lou 24712374
+//Victoria Avgoustis 27529198
+//Laura Elena González 27217323	
+//
+//COMP 249 - Section D
+// This class contains a couple of methods and a constructor that will allow 
+// us to create a Hash table/Array of linked list. We realize here that by creating 
+// an array of linked list, it is a more efficient was of searching through
+// these linked list by classifying them by their serial numbers with the MOD 
+// calculation. Above each method the will be a brief description on what the method 
+// does and how.
+//---------------------------------------------------------------------------------
+
 import java.io.*;
 import java.util.Scanner;
 
@@ -10,11 +28,17 @@ public class CellHash {
 	public CellHash(){
 		hashArr = new CellList[size];
 	}
-	//Takes serial number from an object and calculates where it will be placed in out hashArray//
+	// Takes serial number from an object and calculates where it will be placed in out hashArray
+	// by calculating the MOD 12 of the serial number so that we get a index from 0-11
+	
 	public int computeHashValue(long serialNum){
 		int index = (int)serialNum%12;
 		return index;	
 	}
+	// The addToHashTable method, takes a text file, reads it and initializes cellPhone objects 
+	// that will then be placed in a linked list (provided it isn't a duplicated serial number)
+	// and is then placed in an index of the array. The index location is provided by the 
+	// computeHashValue method above.
 	
 	public void addToHashTable(String cellFile){
 		try{
@@ -39,11 +63,15 @@ public class CellHash {
 			
 			inputStream.close();
 		}
+		// If the file read isn't found will terminate program
 		catch(IOException e){
 			System.out.println("File not found!");
 			System.exit(0);
 		}		
 	}
+	// DisplayHashContents() method goes throught the newly created array of linked list
+	// and with the help of the showContents method, prints out each cellList found in each
+	// array index (if any).
 	
 	public void displayHashContents(){
 		int i =0;
@@ -62,7 +90,10 @@ public class CellHash {
 		}
 		System.out.println();
 	}
-	
+	// findCell() method takes in a serial number and verifies whether it is found in a linked list
+	// found in the HashArray. As opposed to going through every single array index, it will only
+	// go through the index provided by the computeHashValue() method for a quicker and more effiecient
+	// search as shown via the counter. 
 	
 	public void  findCell(long serialNum){			
 		int location = computeHashValue(serialNum); 		
--- CellHashUtilization.java
+++ CellHashUtilization.java
@@ -1,23 +1,46 @@
+//--------------------------------------------------------------------------------
+//Assignment 3
+//Part 2
+//Written by: 
+//Sviki Gabbay - 27490968
+//Andrés Lou 24712374
+//Victoria Avgoustis 27529198
+//Laura Elena González 27217323	
+//
+//COMP 249 - Section D
+// This is our driver class for our CellHash class. Here we read a file and  
+// initialize cellPhone objects to be placed in a linked list that are then placed 
+// in a HashArray. 
+//---------------------------------------------------------------------------------
+
 import java.util.Scanner;
 
 public class CellHashUtilization {
 
 	public static void main(String[] args) {
 		
+	// Prompt user for the file they wish to be used//
 		Scanner keyboard = new Scanner(System.in);
 		System.out.println("Please enter the name of the cellphone file: ");		
 		String fileName = keyboard.next();
 		
+	// Create our HashArray of linked list followed by initializing it.
 		CellHash hashArr = new CellHash();
 		hashArr.addToHashTable(fileName);
 		
+	// Display the HAshArray	
 		hashArr.displayHashContents();
-		//computeHashValue(phone1.getSerialNum());
-
+		
+	// Prompt User for a serial number that will be used to see if it
+	// exists in the linked list.	
 		System.out.println("Please enter a serial you wish to search: ");
 		long newSerial = keyboard.nextLong();
-		
+	
+	// We search through the linked list for the serial number.	
+	// We also display how many searches it took and when compared to the other find method,
+	// it's clear to see this approach is faster and more efficient.
 		hashArr.findCell(newSerial);
+		
 		keyboard.close();
 	}
 
